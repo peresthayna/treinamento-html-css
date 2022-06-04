a5aab3b7726beb6a962566f3d571b2014c22f1f3
Thayna Peres <thaynapamela27@gmail.com>
2022-06-04 19:15:38 -0300

    página concluida

---
+++ js/script.js
@@ -0,0 +1,12 @@
+  
+$('#main-slider').slick(
+  {
+    dots: true, 
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  }
+);
